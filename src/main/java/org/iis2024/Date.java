9214aece489cd61e997e8bdd0f4325eea34c7c8a
Alvaro2402 <alarcon.rodriguez.alv@gmail.com>
2026-03-24 17:10:03 +0100

    Changed Constructor

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,7 +20,7 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-  private int hour;;
+  private int hour;
 
   /**
    * Retrieves the day component of this date.
